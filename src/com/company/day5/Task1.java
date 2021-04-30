4214bbc15bfec2b68f461adff4f4d9e1a12a1f29
NarekBabayan <narek.babayan00@mail.ru>
2021-04-30 13:15:24 +0400

    change task

--- src/com/company/day5/Task1.java
+++ src/com/company/day5/Task1.java
@@ -6,25 +6,25 @@ public class Task1 {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
-        int myValue=(number+44)/44;
+        int myValue=(number+100)/100;
 
 
 
         switch (myValue){
             case 1:{
-                System.out.println("0-44");
+                System.out.println("0-100");
                 break;
             }
             case 2:{
-                System.out.println("45-89");
+                System.out.println("100-200");
                 break;
             }
             case 3:{
-                System.out.println("90-134");
+                System.out.println("200-300");
                 break;
             }
             default:{
-                System.out.println("number or >135 or <0");
+                System.out.println("number or >300 or <0");
             }
         }
 
